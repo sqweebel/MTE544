58b01f6860e3081c19eebed316a9af95922f547a
sqweebel <colin.g.evans96@gmail.com>
2018-11-07 20:11:08 -0500

    Add files via upload

--- turtlebot_example_pos.cpp
+++ turtlebot_example_pos.cpp
@@ -11,6 +11,7 @@
 // //////////////////////////////////////////////////////////
 
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
@@ -22,8 +23,10 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseArray.h>
 #include <std_msgs/Header.h>
+#include <tf2/LinearMath/Quaternion.h>
 #include <random>
 #include <iostream>
+#include <fstream>
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
@@ -31,6 +34,15 @@
 ros::Publisher pose_publisher;
 ros::Publisher pose_array_publisher;
 ros::Publisher marker_pub;
+
+
+struct quat{
+	double w;
+	double x;
+	double y;
+	double z;
+
+};
 
 double ips_x;
 double ips_y;
@@ -41,8 +53,10 @@ double odom_yaw;
 double odom_vx;
 double odom_vy;
 double odom_az;
-
 time_t timer;
+tf::Matrix3x3 mat;
+tf2::Quaternion myQ;
+quat q;
 
 short sgn(int x) { return x >= 0 ? 1 : -1; }
 
@@ -76,10 +90,25 @@ void odom_callback(const nav_msgs::Odometry &msg){
     if(odom_yaw<0){ //need to map
  	odom_yaw += 2*M_PI;
     }
-    
-
 }
 
+quat e2q(double pitch, double roll, double yaw){
+
+	double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+
+    quat q_temp;
+    q_temp.w = cy * cr * cp + sy * sr * sp;
+    q_temp.x = cy * sr * cp - sy * cr * sp;
+    q_temp.y = cy * cr * sp + sy * sr * cp;
+    q_temp.z = sy * cr * cp - cy * sr * sp;
+    return q_temp;
+
+}
 //Callback function for the Position topic (LIVE)
 /*
 void pose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
@@ -151,21 +180,21 @@ float normpdf(float meas, float mean, float sigma){
 }
 
 
-int P = 10;  //Number of particles
+int P = 100;  //Number of particles
 Eigen::MatrixXf state_previous(3,1);
 Eigen::MatrixXf state_current(3,1);
 Eigen::MatrixXf state_update(3,1);
 Eigen::MatrixXf meas(3,1);
 Eigen::MatrixXf e(3,1);
 Eigen::MatrixXf d(3,1);
+
 Eigen::MatrixXf X(3,P);
 Eigen::MatrixXf w(3,P);
 Eigen::MatrixXf W(3,P);
+//float W;
 std::default_random_engine generator;
-float R = 0.5;
-float Q = 0.5;
-
-
+float R = 0.1;
+float Q = 0.1;
 
 Eigen::MatrixXf Xp(3,P);
 float V;
@@ -174,12 +203,17 @@ float mean_x;
 float mean_y;
 float mean_z;
 float dt = 0.05;
+float seed;
 
 geometry_msgs::Pose pose; 
 geometry_msgs::PoseArray pose_array;
 
 
 int main(int argc, char **argv) {
+    std::ofstream logFile;
+    logFile.open("/home/colin/ParticleFilter.txt");
+
+
     //Initialize the ROS framework
     ros::init(argc, argv, "main_control");
     ros::NodeHandle n;
@@ -192,6 +226,7 @@ int main(int argc, char **argv) {
     std::normal_distribution<double> distribution_e(0,R);
     std::normal_distribution<double> distribution_d(0,Q);
     std::uniform_real_distribution<double> distribution_i(-1,1);
+    std::uniform_real_distribution<double> rand(0,1);
 
     //Setup topics to Publish from this node
     ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
@@ -202,7 +237,6 @@ int main(int argc, char **argv) {
 
     //Velocity control variable
     geometry_msgs::Twist vel;
-
 
     for(int i=0;i<P;i++){
         X(0,i) = distribution_i(generator);
@@ -215,26 +249,22 @@ int main(int argc, char **argv) {
 
     //Initialize state
 
-    float ips_xi;
 
     state_previous(0) = 0;
     state_previous(1) = 0;
     state_previous(2) = 0;
-    bool started = 0;
     float mean_x = 0;
     float var_x = 0;
 
     while (ros::ok()) {
-	
 
 	        //Main loop code goes here:
-		    V = 0.3;
-		    U = 0.6;
+		    V = 0.2;
+		    U = 0.2;
 	        vel.linear.x = V;  // set linear speed
 	        vel.angular.z = U; // set angular speed
-
+	        //vel.linear.y = V;
 	        velocity_publisher.publish(vel); // Publish the command velocity
-
 
 		//Motion model update
 
@@ -243,11 +273,12 @@ int main(int argc, char **argv) {
 		    e(2) = distribution_e(generator);
 
 	        state_current(0) = state_previous(0) + odom_vx*cos(ips_yaw)*0.05;
-		    ROS_INFO("Current x: %f", state_current(0));
-		    ROS_INFO("x: %f, y: %f, yaw: %f", ips_x,ips_y,ips_yaw);
-		    state_current(1) = odom_y + e(1);
-		   	state_current(2) = odom_yaw + e(2);
-			
+	        state_current(1) = state_previous(1) + odom_vx*sin(ips_yaw)*0.05;
+	        state_current(2) = state_previous(2) + odom_az*0.05;
+	        if(state_current(2)>2*M_PI){
+	        	state_current(2)=state_current(2)-2*M_PI;
+	        }
+
 		//Measurement update
 
 		    d(0) = distribution_d(generator);
@@ -256,80 +287,105 @@ int main(int argc, char **argv) {
 		
 	        meas(0) = ips_x + d(0);
 		    meas(1) = ips_y + d(1);
-		    meas(2) = ips_yaw + d(2);	
+		    meas(2) = ips_yaw + d(2);		
 		    int counter_x = 0;
 		    float sum_x = 0;
 			//Particle filter
 		    for(int i=0;i<P;i++){
-		        e(0) = distribution_e(generator);
+		        e(0) = distribution_e(generator); //Updates the state space model
 		        e(1) = distribution_e(generator);
 		        e(2) = distribution_e(generator);
 
-
-
-		        Xp(0,i) = X(0,i) + odom_vx*cos(ips_yaw) + e(0);
-		        Xp(1,i) = X(1,i) + e(1);
-		        Xp(2,i) = X(2,i) + e(2); 
-		    	ROS_INFO("meas: %f, mean: %f, sigma: %f", meas(0),Xp(0,i),Q);
-		        w(0,i) = normpdf(meas(0),Xp(0,i),Q);
-		        ROS_INFO("Weighting: %f", w(0,i));
-	
-
-		        if(w(0,i) > 0.2){ 
-		        	
-				    X(0,i) = Xp(0,i);
-				    sum_x += Xp(0,i);
-				    counter_x += 1;
+		        Xp(0,i) = X(0,i) + odom_vx*cos(ips_yaw)*0.05 + e(0);
+		        Xp(1,i) = X(1,i) + odom_vx*sin(ips_yaw)*0.05 + e(1);
+		        Xp(2,i) = X(2,i) + odom_az*0.05 + e(2); 
+		        if(Xp(2,i)>2*M_PI){
+	        		Xp(2,i)=Xp(2,i)-2*M_PI;
+	        	}
+		        w(0,i) = normpdf(meas(0),Xp(0,i),Q); //Generates the particle distribution function
+		        w(1,i) = normpdf(meas(1),Xp(1,i),Q);
+		        w(2,i) = normpdf(meas(2),Xp(2,i),Q);
+		        if(i==0){ //cumulative sum
+		        	W(0,i) = w(0,i);
+		        	W(1,i) = w(1,i);
+		        	W(2,i) = w(2,i);
 		        }
-		        else if(w(0,i) <= 0.2){
-			    	X(0,i) = 0;
+		        else{
+		        	W(0,i) = W(0,i-1) + w(0,i);
+		        	W(1,i) = W(1,i-1) + w(1,i);
+		        	W(2,i) = W(2,i-1) + w(2,i);
 		        }
 		    }
-		    std::normal_distribution<double> distribution_n1(mean_x,var_x);
-		    if(counter_x == 0){
-		        ROS_INFO("Deprived of particles.");
-				for(int i=0;i<P;i++){
-			    	X(0,i) = distribution_n1(generator);
-				}
+		    for(int i=0;i<P;i++){ //Take particle that is correct for our range and add it to the particle set
+		    	seed = rand(generator);
+		    	//ROS_INFO("seed: %f", seed);
+		    	for(int j=0;j<P;j++){
+		    		//seed = rand(generator);
+		    		if(W(0,j)>W(0,P-1)*seed){
+		    			X(0,i) = Xp(0,j);
+						break;
+		    		}
+		    	}
+		    	//ROS_INFO("Continuing...");
+		    	for(int j=0;j<P;j++){
+		    		//seed = rand(generator);
+		    	
+                    if(W(1,j)>W(1,P-1)*seed){
+        
+                    	X(1,i) = Xp(1,j);
+                    	break;
+                    }
+		    	}
+		    	for(int j=0;j<P;j++){
+		    		//seed = rand(generator);
+		    	
+                    if(W(2,j)>W(2,P-1)*seed){
+                	
+                    	X(2,i) = Xp(2,j);
+                    	break;
+                    }
+		    	}
 		    }
-		    else if(counter_x>0){
-		        mean_x = sum_x/counter_x;
-		        sum_x = 0;
-		        counter_x = 0;
-		        for(int i=0;i<P;i++){
-			    	if(X(0,i) != 0){
-	   		        	sum_x += pow(abs(X(0,i)-mean_x),2);
-			        	counter_x += 1;
-			    	}
-			    	else if(X(0,i)==0){
-						X(0,i) = distribution_n1(generator);
-					}
-			    }
-			    var_x = sum_x/counter_x;
-	 	    }
-			ROS_INFO("Counter: %d",counter_x);
 
-	        std::normal_distribution<double> distribution_n2(mean_x,var_x);
-			for(int i=0;i<P;i++){
-			    if(X(0,i)==0){
-					X(0,i) = distribution_n2(generator);
-			    }
-			}
 			pose_array.poses.clear();
 			for(int i=0;i<P;i++){
 				pose.position.x = X(0,i);
-			    pose_array.poses.push_back(pose);
-			}
-			
-			ROS_INFO("Mean: %f, Var: %f, Sum: %f", mean_x, var_x, sum_x);
+				pose.position.y = X(1,i);
+				
 
-		   
-	
-		
+				q = e2q(0,0,X(2,i));
+				pose.orientation.x = q.x;
+				pose.orientation.y = q.y;
+				pose.orientation.z = q.z;
+				pose.orientation.w = q.w;
+
+			    pose_array.poses.push_back(pose);			    
+			}
+
+
+			//ROS_INFO("Mean: %f, Var: %f, Sum: %f", mean_x, var_x, sum_x);
+	float xSum = 0;
+	float ySum = 0;
+	for(int i=0;i<P;i++){
+		xSum += X(0,i);
+		ySum += X(1,i);
+	}
+	float xMean = xSum/P;
+	float yMean = ySum/P;
+
+
+
+	//logFile << "X Position: " << ips_x << "\n";
+	//logFile << "Mean        " << mean << "\n\n";
+
+	logFile << ips_x << ","<<xMean<<"\n";
+
 	pose_array.header.frame_id = "/base_link";
 	pose_array_publisher.publish(pose_array);
-
+	ROS_INFO("mean: %f, actual: %f, current state: %f",yMean,ips_x,state_current(0));
 	state_previous(0) = state_current(0);
+	state_previous(1) = state_current(1);
+	state_previous(2) = state_current(2);
 
     loop_rate.sleep(); //Maintain the loop rate
     ros::spinOnce();   //Check for new messages
