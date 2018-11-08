6af710cb5c7f610e9003228f8650cb7c74dd3200
sqweebel <colin.g.evans96@gmail.com>
2018-11-08 14:51:47 -0500

    Add files via upload

--- turtlebot_example_map.cpp
+++ turtlebot_example_map.cpp
@@ -28,40 +28,38 @@
 ros::Publisher pose_publisher;
 ros::Publisher marker_pub;
 
-double ips_x;
-double ips_y;
-double ips_yaw;
+double ips_x; //x position from IPS
+double ips_y; //y position from IPS
+double ips_yaw; //Yaw (aka angle relative to z-axis) from IPS
 
-double angle_min;
-double angle_max;
-double angle_increment;
-double time_increment;
-double scan_time;
-double range_min;
-double range_max;
-std::vector<float> ranges;
-float res = 5;
-int gridSize = 10*res;
-
-
-nav_msgs::OccupancyGrid beliefMap;
-nav_msgs::MapMetaData map_metadata = beliefMap.info;
+double angle_min; //Lower bound for field of vision of laser scan
+double angle_max; //Upper bound for field of vision of laser scan
+double angle_increment; //Angular distance between each consecutive laser beam
+double time_increment; //Not important (i think)
+double scan_time; //Not important
+double range_min; //Minimum range that laser can travel (i don't use in this code, maybe i should)
+double range_max; //Maximum range that laser can travel (also not used)
+std::vector<float> ranges; //Vector of floats that will contain the distances each laser travelled
+float res = 5; //Resolution of the occupancy grid. Note that I assume a base size of 10x10 - and then the resolution splits each grid into subgrids
+               //So for example, if resolution is 5, each single grid is broken into a 5x5 subgrid, so the resulting map is then 50x50
+int gridSize = 10*res; //Per the previous comment, this value determines the size of the occupancy grid
 
 
+nav_msgs::OccupancyGrid beliefMap; //This is the map which will be updated and published
+nav_msgs::MapMetaData map_metadata = beliefMap.info; //The map topic has a metadata field which contains things like how big it is 
 
+short sgn(int x) { return x >= 0 ? 1 : -1; } //If x>=0, return 1, else return -1
 
-short sgn(int x) { return x >= 0 ? 1 : -1; }
-//Callback function for the Position topic (SIMULATION)
-void pose_callback(const gazebo_msgs::ModelStates &msg) {
-
+void pose_callback(const gazebo_msgs::ModelStates &msg) { //Callback function for the Position topic (SIMULATION)
     int i;
     for (i = 0; i < msg.name.size(); i++)
         if (msg.name[i] == "mobile_base")
             break;
 
-    ips_x = msg.pose[i].position.x + 5; //Add 5 to change coordinate frame
+    ips_x = msg.pose[i].position.x + 5; // In the simulation, the point (0,0) is at the center of the grid. I find it easier if we map (0,0) to the bottom left of the grid, which
+                                        // is the equivalent transformation of shifting the (-5,-5) to (0,0), which is what is done here
     ips_y = msg.pose[i].position.y + 5;
-    ips_yaw = tf::getYaw(msg.pose[i].orientation);
+    ips_yaw = tf::getYaw(msg.pose[i].orientation); //The orientation field of the pose topic is a quaternion. The function tf::getYaw converts the quaternion into yaw
 }
 
 //Callback function for the Position topic (LIVE)
@@ -75,15 +73,15 @@ void pose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
 }*/
 
 //Callback function for the map
-void map_callback(const nav_msgs::OccupancyGrid &msg) {
+void map_callback(const nav_msgs::OccupancyGrid &msg) { //Not sure how we should use this
     //This function is called when a new map is received
 
     //you probably want to save the map into a form which is easy to work with
 }
 
-void scan_callback(const sensor_msgs::LaserScan &msg){
+void scan_callback(const sensor_msgs::LaserScan &msg){ //This function is called when laser scan data is received
 
-    angle_min = msg.angle_min;
+    angle_min = msg.angle_min; //See where these variables are declared above to know what they represent
     angle_max = msg.angle_max;
     angle_increment = msg.angle_increment;
     scan_time = msg.scan_time;
@@ -98,7 +96,7 @@ void scan_callback(const sensor_msgs::LaserScan &msg){
 // Usage: (x0, y0) is the first point and (x1, y1) is the second point. The calculated
 //        points (x, y) are stored in the x and y vector. x and y should be empty
 //	  vectors of integers and shold be defined where this function is called from.
-void bresenham(int x0, int y0, int x1, int y1, std::vector<long int> &x, std::vector<long int> &y) {
+void bresenham(int x0, int y0, int x1, int y1, std::vector<long int> &x, std::vector<long int> &y) { // Bresenham algorithm which was coded for us
 
     int dx = abs(x1 - x0);
     int dy = abs(y1 - y0);
@@ -141,179 +139,130 @@ void bresenham(int x0, int y0, int x1, int y1, std::vector<long int> &x, std::ve
     }
 }
 
-
-
 int main(int argc, char **argv) {
     //Initialize the ROS framework
-    ros::init(argc, argv, "main_control");
-    ros::NodeHandle n;
-    tf::Transform transform;
+    ros::init(argc, argv, "mapping");
+    ros::NodeHandle n_map;
+    tf::Transform transform; //This transform is used to map our occupancy grid to the base frame in rviz
     tf::TransformBroadcaster br;
-    std::ofstream logFile1;
-    std::ofstream logFile2;
-    logFile1.open("/home/colin/angles.txt");
-    logFile2.open("/home/colin/map.txt");
-    
-    Eigen::MatrixXf map(gridSize, gridSize);
-    map.setZero();
-   
-    //Eigen::MatrixXd map0 = map;
+
+    int angles; //Number of angles in each set of scan data
+    int x0; //See bresenham
+    int y0;
+    int x1;
+    int y1;
+    float V; //Linear velocity of robot - note this is relative to the robot frame
+    float U; //Angular velocity of robot
+    double angle; //See below
+    std::vector<long int> x; //The vectors x and y are acted on by the bresenham function. They are vectors which correspond to the cells in the map through which a laser travelled
+    std::vector<long int> y;
+    double size;
+    map_metadata.resolution = 1/res; //All this metadata is a description of our map
+    map_metadata.width = 10*res;
+    map_metadata.height = 10*res;
+    map_metadata.origin.position.x = 0;
+    map_metadata.origin.position.y = 0;
+    map_metadata.origin.position.z = 0;
+    map_metadata.origin.orientation.x = 0;  
+    map_metadata.origin.orientation.y = 0;
+    map_metadata.origin.orientation.z = 0;
+    beliefMap.info = map_metadata;
+
+    Eigen::MatrixXf map(gridSize, gridSize); //Initialize map
+    map.setZero(); //Set all probabilities in map to 0 initially
+
     //Subscribe to the desired topics and assign callbacks
-    
-    ros::Subscriber pose_sub = n.subscribe("/gazebo/model_states", 1, pose_callback);
-    ros::Subscriber map_sub = n.subscribe("/map", 1, map_callback);
-    ros::Subscriber scan_sub = n.subscribe("/scan",1,scan_callback);
+    ros::Subscriber pose_sub = n_map.subscribe("/gazebo/model_states", 1, pose_callback); //Subscribe to IPS from gazebo
+    //ros::Subscriber map_sub = n_map.subscribe("/map", 1, map_callback); //Not used
+    ros::Subscriber scan_sub = n_map.subscribe("/scan",1,scan_callback); //Subscribe to the laser scans
 
     //Setup topics to Publish from this node
-    ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
-    pose_publisher = n.advertise<geometry_msgs::PoseStamped>("/pose", 1, true);
-    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
-    ros::Publisher map_pub = n.advertise<nav_msgs::OccupancyGrid>("/beliefMap",1,true);
-    ros::Publisher meta_map_pub = n.advertise<nav_msgs::MapMetaData>("/beliefMap_metadata",1,true);
+    ros::Publisher velocity_publisher = n_map.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1); //Velocity publisher
+    //pose_publisher = n_map.advertise<geometry_msgs::PoseStamped>("/pose", 1, true); //Not used here
+    //marker_pub = n_map.advertise<visualization_msgs::Marker>("visualization_marker", 1, true); //Also not used
+    ros::Publisher map_pub = n_map.advertise<nav_msgs::OccupancyGrid>("/beliefMap",1,true); //Map publisher
 
     //Velocity control variable
     geometry_msgs::Twist vel;
-    //Set the loop ratehttps://en.wikipedia.org/wiki/Row-_and_column-major_order
+    //Set the loop rate
     ros::Rate loop_rate(20); //20Hz update rate
 
-    //std::vector<std::vector<int>> mapArray(row, std::vector<int>(col));
-    int count = 0;
-    float V;
-    float U;
+
     while (ros::ok()) {
 
-
-        //Main loop code goes here:
-        //vel.linear.x = 0;  // set linear speed
-        //vel.angular.z = 0; // set angular speed
-        //velocity_publisher.publish(vel); // Publish the command velocity
 
         loop_rate.sleep(); //Maintain the loop rate
         ros::spinOnce();   //Check for new messagess
 
-   //Main loop code goes here:
-            
+    	transform.setOrigin(tf::Vector3(5,5,0));
+    	transform.setRotation(tf::Quaternion(0,0,0,1));
+    	br.sendTransform(tf::StampedTransform(transform,ros::Time::now(),"/map","base_link")); //These lines transform our map so it overlays the grid shown in rviz
 
-	transform.setOrigin(tf::Vector3(5,5,0));
-	transform.setRotation(tf::Quaternion(0,0,0,1));
-	br.sendTransform(tf::StampedTransform(transform,ros::Time::now(),"/map","base_link"));
+    	if(!ranges.empty()){ //Initially, the ranges value is empty, so if that's the case do not enter main body of code
+    	    
+    	    angles = round((angle_max - angle_min)/angle_increment); //Find number of angles through which to loop
+           	    //ROS_INFO("# Angles: %d", angles);
+            for(int i=0;i<angles;i++){ //Loop through all the angles
+    		
+        	 	angle = (ips_yaw + angle_min) + i*angle_increment; //Pick an angle
+        		x.clear(); //Clear x and y so we don't retain old values
+         	   	y.clear();
 
-	if(!ranges.empty()){
-	    int angles;
-	    int x0;
-	    int y0;
-   	    int x1;
- 	    int y1;
-	    double angle;
-	    std::vector<long int> x;
-	    std::vector<long int> y;
-	    double size;
-	    angles = round((angle_max - angle_min)/angle_increment);
-       	    //ROS_INFO("# Angles: %d", angles);
-            for(int i=0;i<angles;i++){
-		
-	 	angle = (ips_yaw + angle_min) + i*angle_increment;
-		x.clear();
- 	   	y.clear();
+    
+        		if(!(std::isnan(ranges[i]))){ //If a laser doesn't hit anything, NaN is returned - so no need to update map for range of NaN
+        		    x0 = floor(ips_x*res);
+        		    y0 = floor(ips_y*res);
+        		    x1 = floor((ips_x + ranges[i]*cos(angle))*res);
+        		    y1 = floor((ips_y + ranges[i]*sin(angle))*res); //These lines obtain the position of the robot and the position of the object which a laser hit
+                                                                    //
 
-   		
-		if(!(std::isnan(ranges[i]))){
-		    logFile1 << angle << ": " << ranges[i] << "\n";
-		    x0 = floor(ips_x*res);
-		    y0 = floor(ips_y*res);
-		    x1 = floor((ips_x + ranges[i]*cos(angle))*res);
-		    y1 = floor((ips_y + ranges[i]*sin(angle))*res);
-		    logFile1 << x0 << ", " << y0 << ", " << x1 << ", " << y1 << "\n";
-
-		    bresenham(x0,y0,x1,y1,x,y);
+        		    bresenham(x0,y0,x1,y1,x,y);
                     Eigen::ArrayXXf bresenhamResult(x.size(),3);
-		    
-		    for(int j = 0; j<x.size();j++){//Map bresenham x and y vectors to bresenham results map                     
+        		    
+        		    for(int j = 0; j<x.size();j++){//Map bresenham x and y vectors to bresenham results map                     
 
                         bresenhamResult(j, 0) = x[j];
 
                         bresenhamResult(j, 1) = y[j];
 
                         if(j == x.size()-1){
-                            bresenhamResult(j,2) = 99;
+                            bresenhamResult(j,2) = 60;
                         }
                         else{
-                            bresenhamResult(j,2) = 1;
+                            bresenhamResult(j,2) = 40;
                         }
-			    if(y[j]>(10*res - 1)){
-				y[j] = 10*res - 1;
-			    }
-			    if(x[j]>(10*res - 1)){
-				x[j] = 10*res - 1;
-			    }
-			    if(y[j]<0){
-			    	y[j] = 0;
-			    }
-			    if(x[j]<0){
-				x[j] = 0;
-			    }
-			/*
-			    if(map(19-y[j],x[j]) == 100){
-				//do not alter
-			    }
-			    else{
-                            map(19-y[j], x[j]) = map(19-y[j], x[j]) + log(bresenhamResult(j,2)/(100-bresenhamResult(j,2)));
-			    }
-			    if(map(19-y[j],x[j])>=1){
-			    	map(19-y[j], x[j]) = 100;
-			    }
-			    if(map(19-y[j],x[j])<0){
-				map(19-y[j],x[j]) = 0;
-			    }    
-			    
-			*/
-
-			    else{
+        			    if(y[j]>(10*res - 1)){
+        				    y[j] = 10*res - 1;
+        			    }
+        			    if(x[j]>(10*res - 1)){
+        				    x[j] = 10*res - 1;
+        			    }
+        			    if(y[j]<0){
+        			    	y[j] = 0;
+        			    }
+        			    if(x[j]<0){
+        				    x[j] = 0;
+        			    }
+        			    else{
                             map(y[j], x[j]) = map(y[j], x[j]) + log(bresenhamResult(j,2)/(100-bresenhamResult(j,2)));
-			    }
-			    if(map(y[j],x[j])>=100){
-			    	map(y[j], x[j]) = 100;
-			    }
-			    if(map(y[j],x[j])<0){
-				map(y[j],x[j]) = 0;
-			    }   
-
-			    beliefMap.data.clear();
-			    for(int p=0;p<map.cols();p++){
-			        for(int l=0;l<map.rows();l++){
-				    beliefMap.data.push_back(map(p, l));
-				}
- 			    }    
-			    map_metadata.resolution = 1/res;
-			    map_metadata.width = 10*res;
-			    map_metadata.height = 10*res;
-     			    map_metadata.origin.position.x = 0;
-     			    map_metadata.origin.position.y = 0;
-     			    map_metadata.origin.position.z = 0;
-     			    map_metadata.origin.orientation.x = 0;	
-     			    map_metadata.origin.orientation.y = 0;
-     			    map_metadata.origin.orientation.z = 0;
-			    beliefMap.info = map_metadata;
-			    map_pub.publish(beliefMap); 	
-                    } 
-		}		
-		
-	    } 
-	//break;	
-	}	
+        			    }
+        			    if(map(y[j],x[j])>=100){
+        			    	map(y[j], x[j]) = 100;
+        			    }
+        			    if(map(y[j],x[j])<0){
+        				    map(y[j],x[j]) = 0;
+        			    }   
+        			    beliefMap.data.clear();
+        			    for(int p=0;p<map.cols();p++){
+        			        for(int l=0;l<map.rows();l++){
+        				        beliefMap.data.push_back(map(p, l));
+                            }
+        				}    
+                    }    
+        		    map_pub.publish(beliefMap); 	
+                } 
+		    }				
+	    } 	
     }
-	map(9,9) = 9;
-
-	for(int j = 0; j < map.rows(); j++){
-	    for(int k = 0; k < map.rows(); k++){
-		logFile2 << map(j, k) << " ";
-	     }
-	       logFile2 << "\n";
-	}
-	for(int i=0;i<400;i++){
-	    logFile2 << std::to_string(beliefMap.data[i]) << " ";
-	}
-	logFile1.close();
-	logFile2.close();
     return 0;
 }
--- turtlebot_example_pos.cpp
+++ turtlebot_example_pos.cpp
@@ -35,28 +35,20 @@ ros::Publisher pose_publisher;
 ros::Publisher pose_array_publisher;
 ros::Publisher marker_pub;
 
-
 struct quat{
 	double w;
 	double x;
 	double y;
 	double z;
-
 };
 
 double ips_x;
 double ips_y;
 double ips_yaw;
-double odom_x;
-double odom_y;
-double odom_yaw;
 double odom_vx;
 double odom_vy;
 double odom_az;
-time_t timer;
-tf::Matrix3x3 mat;
-tf2::Quaternion myQ;
-quat q;
+
 
 short sgn(int x) { return x >= 0 ? 1 : -1; }
 
@@ -79,17 +71,9 @@ void pose_callback(const gazebo_msgs::ModelStates &msg) {
 }
 
 void odom_callback(const nav_msgs::Odometry &msg){
-    
-    odom_x = msg.pose.pose.position.x;
-    odom_y = msg.pose.pose.position.y;
-    odom_yaw = tf::getYaw(msg.pose.pose.orientation);
-
     odom_vx = msg.twist.twist.linear.x;
     odom_vy = msg.twist.twist.linear.y;
     odom_az = msg.twist.twist.angular.z;
-    if(odom_yaw<0){ //need to map
- 	odom_yaw += 2*M_PI;
-    }
 }
 
 quat e2q(double pitch, double roll, double yaw){
@@ -103,6 +87,9 @@ quat e2q(double pitch, double roll, double yaw){
 
     quat q_temp;
     q_temp.w = cy * cr * cp + sy * sr * sp;
+    if(q_temp.w>1){
+    	q_temp.w = 1 - q_temp.w;
+    }
     q_temp.x = cy * sr * cp - sy * cr * sp;
     q_temp.y = cy * cr * sp + sy * sr * cp;
     q_temp.z = sy * cr * cp - cy * sr * sp;
@@ -132,27 +119,21 @@ void map_callback(const nav_msgs::OccupancyGrid &msg) {
 //        points (x, y) are stored in the x and y vector. x and y should be empty
 //	  vectors of integers and shold be defined where this function is called from.
 void bresenham(int x0, int y0, int x1, int y1, std::vector<int> &x, std::vector<int> &y) {
-
     int dx = abs(x1 - x0);
     int dy = abs(y1 - y0);
     int dx2 = x1 - x0;
     int dy2 = y1 - y0;
-
     const bool s = abs(dy) > abs(dx);
-
     if (s) {
         int dx2 = dx;
         dx = dy;
         dy = dx2;
     }
-
     int inc1 = 2 * dy;
     int d = inc1 - dx;
     int inc2 = d - dx;
-
     x.push_back(x0);
     y.push_back(y0);
-
     while (x0 != x1 || y0 != y1) {
         if (s)
             y0 += sgn(dy2);
@@ -167,7 +148,6 @@ void bresenham(int x0, int y0, int x1, int y1, std::vector<int> &x, std::vector<
             else
                 y0 += sgn(dy2);
         }
-
         //Add point to vector
         x.push_back(x0);
         y.push_back(y0);
@@ -178,24 +158,19 @@ void bresenham(int x0, int y0, int x1, int y1, std::vector<int> &x, std::vector<
 float normpdf(float meas, float mean, float sigma){
     return ( 1 / (sigma*sqrt(2*M_PI) ) ) * exp( -0.5 * pow( (meas-mean) / sigma,2));
 }
-
-
-int P = 100;  //Number of particles
+int P = 50;  //Number of particles
 Eigen::MatrixXf state_previous(3,1);
 Eigen::MatrixXf state_current(3,1);
-Eigen::MatrixXf state_update(3,1);
+
 Eigen::MatrixXf meas(3,1);
 Eigen::MatrixXf e(3,1);
 Eigen::MatrixXf d(3,1);
-
 Eigen::MatrixXf X(3,P);
 Eigen::MatrixXf w(3,P);
 Eigen::MatrixXf W(3,P);
-//float W;
 std::default_random_engine generator;
 float R = 0.1;
 float Q = 0.1;
-
 Eigen::MatrixXf Xp(3,P);
 float V;
 float U;
@@ -204,24 +179,19 @@ float mean_y;
 float mean_z;
 float dt = 0.05;
 float seed;
-
+quat q;
 geometry_msgs::Pose pose; 
 geometry_msgs::PoseArray pose_array;
 
-
 int main(int argc, char **argv) {
-    std::ofstream logFile;
-    logFile.open("/home/colin/ParticleFilter.txt");
-
-
     //Initialize the ROS framework
-    ros::init(argc, argv, "main_control");
-    ros::NodeHandle n;
+    ros::init(argc, argv, "positioning");
+    ros::NodeHandle n_pos;
 
     //Subscribe to the desired topics and assign callbacks
-    ros::Subscriber pose_sub = n.subscribe("/gazebo/model_states", 1, pose_callback);
-    ros::Subscriber map_sub = n.subscribe("/map", 1, map_callback);
-    ros::Subscriber odom_sub = n.subscribe("/odom",1,odom_callback);
+    ros::Subscriber pose_sub = n_pos.subscribe("/gazebo/model_states", 1, pose_callback);
+    ros::Subscriber map_sub = n_pos.subscribe("/map", 1, map_callback);
+    ros::Subscriber odom_sub = n_pos.subscribe("/odom",1,odom_callback);
 
     std::normal_distribution<double> distribution_e(0,R);
     std::normal_distribution<double> distribution_d(0,Q);
@@ -229,11 +199,11 @@ int main(int argc, char **argv) {
     std::uniform_real_distribution<double> rand(0,1);
 
     //Setup topics to Publish from this node
-    ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
-    pose_publisher = n.advertise<geometry_msgs::Pose>("/pose", 1, true);
-    pose_array_publisher = n.advertise<geometry_msgs::PoseArray>("/pose_array",1,true);
-    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
-
+    ros::Publisher velocity_publisher = n_pos.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
+    //pose_publisher = n_pos.advertise<geometry_msgs::Pose>("/pose", 1, true);
+    pose_array_publisher = n_pos.advertise<geometry_msgs::PoseArray>("/pose_array",1,true);
+    //marker_pub = n_pos.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
+    pose_array.header.frame_id = "/base_link";
 
     //Velocity control variable
     geometry_msgs::Twist vel;
@@ -249,147 +219,131 @@ int main(int argc, char **argv) {
 
     //Initialize state
 
-
     state_previous(0) = 0;
     state_previous(1) = 0;
     state_previous(2) = 0;
-    float mean_x = 0;
-    float var_x = 0;
 
     while (ros::ok()) {
 
-	        //Main loop code goes here:
-		    V = 0.2;
-		    U = 0.2;
-	        vel.linear.x = V;  // set linear speed
-	        vel.angular.z = U; // set angular speed
-	        //vel.linear.y = V;
-	        velocity_publisher.publish(vel); // Publish the command velocity
+	    loop_rate.sleep(); //Maintain the loop rate
+	    ros::spinOnce();   //Check for new messages
+
+        //Main loop code goes here:
+	    /*V = 0.2;
+	    U = 0.2;
+        vel.linear.x = V;  // set linear speed
+        vel.angular.z = U; // set angular speed
+        velocity_publisher.publish(vel); // Publish the command velocity*/
 
 		//Motion model update
 
-	        e(0) = distribution_e(generator);
-		    e(1) = distribution_e(generator);
-		    e(2) = distribution_e(generator);
+        e(0) = distribution_e(generator);
+	    e(1) = distribution_e(generator);
+	    e(2) = distribution_e(generator);
 
-	        state_current(0) = state_previous(0) + odom_vx*cos(ips_yaw)*0.05;
-	        state_current(1) = state_previous(1) + odom_vx*sin(ips_yaw)*0.05;
-	        state_current(2) = state_previous(2) + odom_az*0.05;
-	        if(state_current(2)>2*M_PI){
-	        	state_current(2)=state_current(2)-2*M_PI;
-	        }
-
+        state_current(0) = state_previous(0) + odom_vx*cos(state_previous(2))*0.05;
+        state_current(1) = state_previous(1) + odom_vx*sin(state_previous(2))*0.05;
+        state_current(2) = state_previous(2) + odom_az*0.05;
+        if(state_current(2)>2*M_PI){
+        	state_current(2)=state_current(2)-2*M_PI;
+        }
+        
 		//Measurement update
 
-		    d(0) = distribution_d(generator);
-		    d(1) = distribution_d(generator);
-		    d(2) = distribution_d(generator);
-		
-	        meas(0) = ips_x + d(0);
-		    meas(1) = ips_y + d(1);
-		    meas(2) = ips_yaw + d(2);		
-		    int counter_x = 0;
-		    float sum_x = 0;
-			//Particle filter
-		    for(int i=0;i<P;i++){
-		        e(0) = distribution_e(generator); //Updates the state space model
-		        e(1) = distribution_e(generator);
-		        e(2) = distribution_e(generator);
+	    d(0) = distribution_d(generator);
+	    d(1) = distribution_d(generator);
+	    d(2) = distribution_d(generator);
+	
+        meas(0) = ips_x + d(0);
+	    meas(1) = ips_y + d(1);
+	    meas(2) = ips_yaw + d(2);		
+		//Particle filter
+	    for(int i=0;i<P;i++){
+	        e(0) = distribution_e(generator); //Updates the state space model
+	        e(1) = distribution_e(generator);
+	        e(2) = distribution_e(generator);
 
-		        Xp(0,i) = X(0,i) + odom_vx*cos(ips_yaw)*0.05 + e(0);
-		        Xp(1,i) = X(1,i) + odom_vx*sin(ips_yaw)*0.05 + e(1);
-		        Xp(2,i) = X(2,i) + odom_az*0.05 + e(2); 
-		        if(Xp(2,i)>2*M_PI){
-	        		Xp(2,i)=Xp(2,i)-2*M_PI;
-	        	}
-		        w(0,i) = normpdf(meas(0),Xp(0,i),Q); //Generates the particle distribution function
-		        w(1,i) = normpdf(meas(1),Xp(1,i),Q);
-		        w(2,i) = normpdf(meas(2),Xp(2,i),Q);
-		        if(i==0){ //cumulative sum
-		        	W(0,i) = w(0,i);
-		        	W(1,i) = w(1,i);
-		        	W(2,i) = w(2,i);
-		        }
-		        else{
-		        	W(0,i) = W(0,i-1) + w(0,i);
-		        	W(1,i) = W(1,i-1) + w(1,i);
-		        	W(2,i) = W(2,i-1) + w(2,i);
-		        }
-		    }
-		    for(int i=0;i<P;i++){ //Take particle that is correct for our range and add it to the particle set
-		    	seed = rand(generator);
-		    	//ROS_INFO("seed: %f", seed);
-		    	for(int j=0;j<P;j++){
-		    		//seed = rand(generator);
-		    		if(W(0,j)>W(0,P-1)*seed){
-		    			X(0,i) = Xp(0,j);
-						break;
-		    		}
-		    	}
-		    	//ROS_INFO("Continuing...");
-		    	for(int j=0;j<P;j++){
-		    		//seed = rand(generator);
-		    	
-                    if(W(1,j)>W(1,P-1)*seed){
-        
-                    	X(1,i) = Xp(1,j);
-                    	break;
-                    }
-		    	}
-		    	for(int j=0;j<P;j++){
-		    		//seed = rand(generator);
-		    	
-                    if(W(2,j)>W(2,P-1)*seed){
-                	
-                    	X(2,i) = Xp(2,j);
-                    	break;
-                    }
-		    	}
-		    }
+	        Xp(0,i) = X(0,i) + odom_vx*cos(state_previous(2))*0.05 + e(0);
+	        Xp(1,i) = X(1,i) + odom_vx*sin(state_previous(2))*0.05 + e(1);
+	        Xp(2,i) = X(2,i) + odom_az*0.05 + e(2); 
+	        ROS_INFO("first yaw: %f", Xp(2,i));
+	        if(Xp(2,i)>2*M_PI){
+        		Xp(2,i)=Xp(2,i)-2*M_PI;
+        	}
+        	if(Xp(2,i)<0){
+        		Xp(2,i) = 2*M_PI + Xp(2,i);
+        	}
+	        w(0,i) = normpdf(meas(0),Xp(0,i),Q); //Generates the particle distribution function
+	        w(1,i) = normpdf(meas(1),Xp(1,i),Q);
+	        w(2,i) = normpdf(meas(2),Xp(2,i),Q);
+	        if(i==0){ //cumulative sum
+	        	W(0,i) = w(0,i);
+	        	W(1,i) = w(1,i);
+	        	W(2,i) = w(2,i);
+	        }
+	        else{
+	        	W(0,i) = W(0,i-1) + w(0,i);
+	        	W(1,i) = W(1,i-1) + w(1,i);
+	        	W(2,i) = W(2,i-1) + w(2,i);
+	        }
+	    }
+	    for(int i=0;i<P;i++){ //Take particle that is correct for our range and add it to the particle set
+	    	seed = rand(generator);
+	    	//ROS_INFO("seed: %f", seed);
+	    	for(int j=0;j<P;j++){
+	    		//seed = rand(generator);
+	    		if(W(0,j)>W(0,P-1)*seed){
+	    			X(0,i) = Xp(0,j);
+					break;
+	    		}
+	    	}
+	    	//ROS_INFO("Continuing...");
+	    	for(int j=0;j<P;j++){
+	    		//seed = rand(generator);
+	    	
+                if(W(1,j)>W(1,P-1)*seed){
+    
+                	X(1,i) = Xp(1,j);
+                	break;
+                }
+	    	}
+	    	for(int j=0;j<P;j++){
+	    		//seed = rand(generator);
+	    	
+                if(W(2,j)>W(2,P-1)*seed){
+            	
+                	X(2,i) = Xp(2,j);
+                	break;
+                }
+	    	}
+	    }
 
-			pose_array.poses.clear();
-			for(int i=0;i<P;i++){
-				pose.position.x = X(0,i);
-				pose.position.y = X(1,i);
-				
+		pose_array.poses.clear();
+		for(int i=0;i<P;i++){
+			pose.position.x = X(0,i);
+			pose.position.y = X(1,i);
+			
+			//Need to map angle back to -pi to pi
 
-				q = e2q(0,0,X(2,i));
-				pose.orientation.x = q.x;
-				pose.orientation.y = q.y;
-				pose.orientation.z = q.z;
-				pose.orientation.w = q.w;
+			if(X(2,i)>2*M_PI){
+				X(2,i) = X(2,i) - 2*M_PI;
+			}	
 
-			    pose_array.poses.push_back(pose);			    
-			}
+			q = e2q(0,0,X(2,i));
+			pose.orientation.x = q.x;
+			pose.orientation.y = q.y;
+			pose.orientation.z = q.z;
+			pose.orientation.w = q.w;
+			ROS_INFO("second yaw: %f, w: %f", X(2,i),q.w);
 
+		    pose_array.poses.push_back(pose);			    
+		}
 
-			//ROS_INFO("Mean: %f, Var: %f, Sum: %f", mean_x, var_x, sum_x);
-	float xSum = 0;
-	float ySum = 0;
-	for(int i=0;i<P;i++){
-		xSum += X(0,i);
-		ySum += X(1,i);
-	}
-	float xMean = xSum/P;
-	float yMean = ySum/P;
+		pose_array_publisher.publish(pose_array);
+		state_previous(0) = state_current(0);
+		state_previous(1) = state_current(1);
+		state_previous(2) = state_current(2);
 
-
-
-	//logFile << "X Position: " << ips_x << "\n";
-	//logFile << "Mean        " << mean << "\n\n";
-
-	logFile << ips_x << ","<<xMean<<"\n";
-
-	pose_array.header.frame_id = "/base_link";
-	pose_array_publisher.publish(pose_array);
-	ROS_INFO("mean: %f, actual: %f, current state: %f",yMean,ips_x,state_current(0));
-	state_previous(0) = state_current(0);
-	state_previous(1) = state_current(1);
-	state_previous(2) = state_current(2);
-
-    loop_rate.sleep(); //Maintain the loop rate
-    ros::spinOnce();   //Check for new messages
-   }
-
+    }
     return 0;
 }
